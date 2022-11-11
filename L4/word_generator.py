dea3d0fafaf05d3eee023e7062d5e1e353791725
Arnav-Jhajharia <jhajhariaarnav@gmail.com>
2022-11-11 13:49:45 +0530

    push

--- L4/word_generator.py
+++ L4/word_generator.py
@@ -3,7 +3,7 @@ import random
 words = []
 
 # for _ in range(0, 5):
-#     words.append([input("Enter word: "), input("Enter hint: ")])
+#     words.append(input("Enter word: "))
 
 
 words = ['awkward', 'awesome', 'jazz', 'hangman', 'quiz']
